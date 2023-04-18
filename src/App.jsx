b4b1f3f43ee3d998c8468fce755d861020785f47
tduncan14 <nbatre1@yahoo.com>
2023-04-18 07:58:05 -0700

    keep moving forward

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
+import { Routes,Route } from "react-router-dom"
+import Home from "./components/views/Home"
+import Collections from "./components/views/Collection"
+
+
 const App = () => {
   return (
-    <div className="min-h-screen">
-      <h4 className="text-3xl">Hello Truffle and Tailwind</h4>
+    <div className="min-h-screen bg-gradient-to-t from-gray-800 bg-repeat">
+
+ 
+   <Routes>
+      < Route path = "/" element ={<Home />} />
+      < Route path = "/collections" element={<Collections />} />
+   </Routes>
+      
+    
     </div>
   )
 }
---
+++ src/components/views/Collection.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+  const Collections = () => {
+
+
+    return (
+        <div>
+
+        </div>
+    )
+  }
+
+
+  export default Collections
